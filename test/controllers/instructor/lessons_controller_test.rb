6032d4dd5fb7830e134217ff9074b379ec618d13
Cynrick Chiu <cyn.2k5@outlook.com>
2015-06-01 05:11:35 +0000

    Set up permissions for lessons, add tests, refactor code

--- test/controllers/instructor/lessons_controller_test.rb
+++ test/controllers/instructor/lessons_controller_test.rb
@@ -8,10 +8,26 @@ class Instructor::LessonsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should not get new when current logged in instructor is not course creator" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    section = FactoryGirl.create(:section)
+    get :new, :section_id => section.id
+    assert_response :unauthorized
+  end
+
   test "create successful" do
     section = FactoryGirl.create(:section)
     sign_in section.course.user
     post :create, :section_id => section.id, :lesson => {:title => 'Ruby', :subtitle => 'Ruby Strings'}
     assert_redirected_to instructor_course_path(section.course)
+  end
+
+  test "create unsuccessful when current logged in instructor is not course creator" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    section = FactoryGirl.create(:section)
+    post :create, :section_id => section.id, :lesson => {:title => 'Ruby', :subtitle => 'Ruby Strings'}
+    assert_response :unauthorized
   end
 end
