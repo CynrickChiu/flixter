99c65e0ad55e37051c5cf67ec174dbb4a28527e7
Cynrick Chiu <cyn.2k5@outlook.com>
2015-06-10 23:32:54 +0000

    Fix tests for enrollments_controller_test.rb and lessons_controller_test.rb

--- test/controllers/enrollments_controller_test.rb
+++ test/controllers/enrollments_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class EnrollmentsControllerTest < ActionController::TestCase
-  test "enroll successfully in a course" do
+  test "enroll successfully in a free course" do
     user = FactoryGirl.create(:user)
-    course = FactoryGirl.create(:course)
+    course = FactoryGirl.create(:course, :cost => 0)
     sign_in user
 
     post :create, :course_id => course.id, :enrollment => {:user_id => user.id}
--- test/controllers/lessons_controller_test.rb
+++ test/controllers/lessons_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class LessonsControllerTest < ActionController::TestCase
-  test "show found" do
-    lesson = FactoryGirl.create(:lesson)
+  test "show lesson if user is enrolled in course" do
+    user = FactoryGirl.create(:user)
+    course = FactoryGirl.create(:course)
+    enrolled = FactoryGirl.create(:enrollment, :user_id => user.id, :course_id => course.id)
+    section = FactoryGirl.create(:section, :course_id => course.id)
+    lesson = FactoryGirl.create(:lesson, :section_id => section.id)
+    sign_in user
     get :show, :id => lesson.id
     assert_response :success
+  end
+
+  test "redirect user to course show page if user is not enrolled in course" do
+    user = FactoryGirl.create(:user)
+    lesson = FactoryGirl.create(:lesson)
+    sign_in user
+    get :show, :id => lesson.id
+    assert_redirected_to course_path(lesson.section.course)
   end
 end
