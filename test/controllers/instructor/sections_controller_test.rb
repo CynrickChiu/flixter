2f7589be2333556595eaa6223e03fdca250b7a5c
Cynrick Chiu <cyn.2k5@outlook.com>
2015-06-12 22:17:48 +0000

    Move new section form into modal, remove original instructor/sections#new tests

--- test/controllers/instructor/sections_controller_test.rb
+++ test/controllers/instructor/sections_controller_test.rb
@@ -1,21 +1,6 @@
 require 'test_helper'
 
 class Instructor::SectionsControllerTest < ActionController::TestCase
-  test "should get new" do
-    course = FactoryGirl.create(:course)
-    sign_in course.user
-    get :new, :course_id => course.id
-    assert_response :success
-  end
-
-  test "should not get new when current logged in instructor is not course creator" do
-    user = FactoryGirl.create(:user)
-    course = FactoryGirl.create(:course)
-    sign_in user
-    get :new, :course_id => course.id
-    assert_response :unauthorized
-  end
-
   test "create successful" do
     course = FactoryGirl.create(:course)
     sign_in course.user
