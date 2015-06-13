d420fb08e7257885c7d3852d42b852e7ad25c5c3
Cynrick Chiu <cyn.2k5@outlook.com>
2015-06-13 03:51:40 +0000

    Move new lesson form into modal, remove original instructor/lessons#new tests

--- test/controllers/instructor/lessons_controller_test.rb
+++ test/controllers/instructor/lessons_controller_test.rb
@@ -1,21 +1,6 @@
 require 'test_helper'
 
 class Instructor::LessonsControllerTest < ActionController::TestCase
-  test "should get new" do
-    section = FactoryGirl.create(:section)
-    sign_in section.course.user
-    get :new, :section_id => section.id
-    assert_response :success
-  end
-
-  test "should not get new when current logged in instructor is not course creator" do
-    user = FactoryGirl.create(:user)
-    sign_in user
-    section = FactoryGirl.create(:section)
-    get :new, :section_id => section.id
-    assert_response :unauthorized
-  end
-
   test "create successful" do
     section = FactoryGirl.create(:section)
     sign_in section.course.user
